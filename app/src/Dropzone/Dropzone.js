4f3bb8bd609e57338861d0f2fdb7fa1a55ed3475
Greg Roques <greg.roques@gmail.com>
2019-07-16 13:32:05 -0400

    almost there

--- app/src/Dropzone/Dropzone.js
+++ app/src/Dropzone/Dropzone.js
@@ -2,17 +2,77 @@ import React, { Component } from 'react'
 import dropzoneStyle from './Dropzone.css'
 
 class Dropzone extends Component {
-    state = {
 
+    state = {
+        highlight: false
     }
+
+    fileInputRef = React.createRef();
+
+    openFileDialog = () =>{
+        if(this.props.disabled) return;
+        this.fileInputRef.current.click();
+    }
+
+    onFilesAdded = e =>{
+        if (this.props.disabled) return;
+        const files = e.target.files;
+        if (this.props.onFilesAdded) {
+            const array = this.fileListToArray(files);
+            this.props.onFilesAdded(array)
+        }
+    }
+    
+    fileListToArray = list =>{
+        const array =[];
+        for (var i = 0; i <list.length; i++) {
+            array.push(list.item(i));
+        }
+        return array;
+    }
+
+    onDragOver = e =>{
+        e.preventDefault();
+        if (this.props.disabled) return;
+        this.setState({ hightlight: true });
+    }
+    
+    onDragLeave = () =>{
+        this.setSTate({ highlight: false })
+    }
+
+    onDrop = e =>{
+        if (this.props.disabled) return;
+        const files = e.dataTransfer.files;
+        if (this.props.onFilesAdded){
+            const array = this.fileListToArray(files);
+            this.props.onFilesAdded(array);
+        }
+        this.setState({ highlight: false })
+    }
+    
 
     render () {
         return (
-            <div className={ dropzoneStyle.Dropzone }>
+            <div 
+                className={ ` ${ dropzoneStyle.Dropzone } this.state.highlight ? ${ dropzoneStyle.highlight } : '' `}
+                onDragOver={ this.onDragOver }
+                onDragLeave={ this.onDragLeave }
+                onDrop={ this.onDrop }
+                onClick={ this.openFileDialog }
+                style={ { cursor: this.props.disabled ? 'default' : 'pointer' }}
+            >
                 <img
                     alt="upload"
                     className={ dropzoneStyle.Icon }
                     src="baseline-cloud_upload-24px.svg" 
+                />
+                <input
+                    ref={ this.fileInputRef }
+                    className= { dropzoneStyle.fileInput }
+                    type='file'
+                    multiple
+                    onChange= { this.onFilesAdded }
                 />
                 <span>Upload Files</span>
             </div>
--- app/src/upload/Upload.js
+++ app/src/upload/Upload.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react'
 import uploadStyle from './Upload.css'
-import Dropzone from '../dropzone/Dropzone'
+import Dropzone from '../Dropzone/Dropzone'
 
 class Upload extends Component {
     state ={
+        files: [],
+        uploading: false,
+        uploadProgress: {},
+        successfullyUploaded: false
+    }
 
+    onFilesAdded = files =>{
+        this.setState(prevState =>({
+            files:prevState.files.concat(files)
+        }))
     }
 
     render() {
         return (
             <div className={ uploadStyle.Upload }>
                 <span className={ uploadStyle.Title }>Upload Files</span>
-                <div className={ uploadStyle.Content}>    
+                <div className={ uploadStyle.Content}>   
+                    <Dropzone
+                        onFilesAdded={ this.onFilesAdded }
+                        disabled={ this.state.uploading || this.state.successfullyUploaded }
+                    /> 
                 </div>
-                <div className={ uploadStyle.Files}>    
+                <div className={ uploadStyle.Files}>  
+                    {this.state.files.map(file =>{
+                        return(
+                            <div key={ file.name } className= { uploadStyle.row }>
+                                <span className={ uploadStyle.fileName }>{ file.name }</span>
+                                { this.renderProgress(file) }
+                            </div>
+                        )
+                    })}  
                 </div>
-                <div className={ uploadStyle.Actions}>    
+                <div className={ uploadStyle.Actions}>  
+                    { this.renderActions() }
                 </div>               
             </div>    
         )
